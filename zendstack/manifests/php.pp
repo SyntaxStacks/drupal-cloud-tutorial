7e5096b7cf030a76f93d50a24d629e76567e03c2
root <root@Jedi.(none)>
2013-04-09 18:01:42 -0500

    Added zend framework include path to default.conf

--- zendstack/manifests/php.pp
+++ zendstack/manifests/php.pp
@@ -1,10 +1,3 @@
 class zendstack::php {
   package { ['php5', 'php5-mysql', 'php5-gd', 'libapache2-mod-php5']: ensure => present, }
-  file { '/etc/php5/cli/php.ini': ensure => present,}
-
-  line { 'include-zend':
-    file => '/etc/php5/cli/php.ini',
-    line => "include_path = ".:/usr/share/php:/opt/ZendFramework/current/library",
-    ensure => present,
-  } 
 }
